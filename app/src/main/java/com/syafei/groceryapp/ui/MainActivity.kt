e5ef2559395d96edcebe3f2a87b02d0608225f68
zhavei <zhavei.vei@gmail.com>
2022-09-23 06:32:55 +0700

    error

--- app/src/main/java/com/syafei/groceryapp/ui/MainActivity.kt
+++ app/src/main/java/com/syafei/groceryapp/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.syafei.groceryapp.ui
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.syafei.groceryapp.R
 import com.syafei.groceryapp.data.local.GroceryDataBase
 import com.syafei.groceryapp.data.local.GroceryItems
 import com.syafei.groceryapp.data.repository.GroceryRepository
@@ -45,7 +49,35 @@ class MainActivity : AppCompatActivity(), GroceryAdapter.GroceryItemClick {
     }
 
     private fun openDilog() {
-        TODO("Not yet implemented")
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.grocery_add_dialog)
+        val cancelBtn = dialog.findViewById<Button>(R.id.btnCencel)
+        val addBtn = dialog.findViewById<Button>(R.id.btnAdd)
+        val itemEt = dialog.findViewById<EditText>(R.id.idTvItemName)
+        val itemPriceET = dialog.findViewById<EditText>(R.id.idEtitemPrice)
+        val itemQUantity = dialog.findViewById<EditText>(R.id.idEtitemQuantity)
+
+        cancelBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+        addBtn.setOnClickListener {
+            val itemName: String = itemEt.text.toString()
+            val itemPrice: String = itemPriceET.text.toString()
+            val itemQuantity: String = itemQUantity.text.toString()
+            val itPrice: Int = itemPrice.toInt()
+            val itQuanty: Int = itemQuantity.toInt()
+            if (itemName.isNotEmpty() && itemPrice.isNotEmpty() && itemQuantity.isNotEmpty()) {
+                val items = GroceryItems(itemName, itPrice, itQuanty)
+                groceryViewModel.insert(items)
+                Toast.makeText(applicationContext, "data inserted", Toast.LENGTH_SHORT).show()
+                groceryAdapter.notifyDataSetChanged()
+                dialog.dismiss()
+            } else {
+                Toast.makeText(applicationContext, "please enter valid - valid", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        dialog.show()
     }
 
     override fun onItemClicked(groceryItems: GroceryItems) {
